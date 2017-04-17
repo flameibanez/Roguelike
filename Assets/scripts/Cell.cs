7a07ff23b23a9ffac0b660af84510fee361a7965
axle12693 <axle12693@gmail.com>
2017-04-17 14:45:22 -0600

    Added Cell, Cell Content, and gameController
    -Renamed camera
    -Added Cell, with ability to determine if traversible
    -Added CellContent, right now only determines if a Cell is traversible
    -Added gameController, includes a test,  will generate map.

---
+++ Assets/scripts/Cell.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cell : MonoBehaviour {
+
+    public enum type
+    {
+        grass,
+        dirt,
+        stone
+    }
+
+    public List<CellContent> contents;
+
+    public bool canWalkHere()
+    {
+        foreach  (CellContent content in contents)
+        {
+            if (!content.canWalkOnThis)
+                return false;
+        }
+        return true;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/gameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+    GameObject f;
+	// Use this for initialization
+	void Start () {
+        f = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        f.AddComponent<Cell>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private Cell Generate(int x, int y, int xSeed, int ySeed)
+    {
+        float height = Mathf.PerlinNoise(x + xSeed, y + ySeed);
+        if (height >= 0.5)
+        {
+            return new Cell();/***************************************************************************/
+        }
+        else
+        {
+            return new Cell();/***************************************************************************/
+        }
+    }
+}
